0983953685b894ce2b9c972937a4247bdc16ed17
yashochiramani <me160003036@iiti.ac.in>
2016-10-27 01:35:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/iit', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'iit.html'));
 });
 
+
+app.get('/past', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'past.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
